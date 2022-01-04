331153907a273d4406b91ed0114fd761304f5da3
softy <leo.menard@epitech.eu>
2022-01-04 22:05:19 +0100

    Update GotoClient.js

--- src/structures/GotoClient.js
+++ src/structures/GotoClient.js
@@ -79,10 +79,17 @@ module.exports = class GotoClient extends AkairoClient {
 
     async start() {
         try {
-            await mongoose.connect(MONGO_STR, {
-                useNewUrlParser: true,
-                useUnifiedTopology: true
-            })
+            if (MONGO_STR) {
+                await mongoose.connect(MONGO_STR, {
+                    useNewUrlParser: true,
+                    useUnifiedTopology: true
+                });
+            } else {
+                await mongoose.connect(process.env.MONGO_STR, {
+                    useNewUrlParser: true,
+                    useUnifiedTopology: true
+                });
+            }
             console.log("DB connectée!");
         } catch (e) {
             console.log("DB non connectée !\n\n", e);
@@ -91,7 +98,11 @@ module.exports = class GotoClient extends AkairoClient {
 
         await this.init();
         try {
-            this.login(TOKEN);
+            if (TOKEN) {
+                this.login(TOKEN);
+            } else {
+                this.login(process.env.TOKEN);
+            }
         } catch (e) {
             console.log("Login erreur\n\n", e);
             return process.exit();
