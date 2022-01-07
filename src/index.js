ce394fda81d57882941c6243242b85f61b899647
softy <leo.menard@epitech.eu>
2022-01-07 21:13:20 +0100

    Update for variable to be load

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const GotoClient = require('./structures/GotoClient');
 const { registerFont } = require('canvas');
 
+require('dotenv').config()
 registerFont('./src/animeace2_bld.ttf', { family: 'Anime Ace 2.0 BB' });
 
 let client = new GotoClient({ prefix: '!' });
